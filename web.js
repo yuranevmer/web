ceceb9a4c45bc863f5366e36703b219db1b61453
yuranevmer <yura.bag@gmail.com>
2013-04-02 10:50:33 +0300

    new

--- web.js
+++ web.js
@@ -1,25 +1,14 @@
 console.log('Hello World');
 var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
 
-  var body = '<html>'+
-    '<head>'+
-    '<meta http-equiv="Content-Type" content="text/html; '+
-    'charset=UTF-8" />'+
-    '</head>'+
-    '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
-    '</form>'+
-    '</body>'+
-    '</html>';
-
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.write(body);
+    res.write(index);
     res.end();
 });
 
-server.listen(80);
+server.listen(8060);
